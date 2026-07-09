d685e1e328cc32378ff3d20554701932fd8a6728
Wesal Ismail <wsalasmayl363@gmail.com>
2026-07-09 04:21:59 +0300

    no need to mention

--- src/pages/FavoriteCity.tsx
+++ src/pages/FavoriteCity.tsx
@@ -3,8 +3,31 @@ import Header from "../components/Header.tsx"
 function FavoriteCity() {
   return (
     <>
-      <Header icon={<i className="bi bi-house-fill"></i>} toHomePage={true}/>
-      <h1>Hello</h1>
+      <Header
+        icon={<i className="bi bi-house-fill"></i>}
+        toHomePage={true}
+      />
+
+      <h2 className="section-title">Favorite City</h2>
+
+      <div className="favorite-city-container">
+        <div className="favorite-city-card">
+          <div className="data-section">
+            <div className="city-name">Jeddah</div>
+
+            <div className="time">12:30 PM</div>
+
+            <div className="description">
+              rainy
+            </div>
+          </div>
+
+          <div className="temp-section">
+            26&deg;C
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }
