a2139676c58321d6fb13ffa2bf49920babd17077
Wesal Ismail <wsalasmayl363@gmail.com>
2026-07-09 04:59:21 +0300

    refactor FavoriteCityCard to use NavLink for navigation

--- src/components/cards/FavoriteCityCard.tsx
+++ src/components/cards/FavoriteCityCard.tsx
@@ -1,19 +1,25 @@
 import { type FavoriteCityCardProps } from "../../types/weather"
-import './FavoriteCityCard.css'
+import { NavLink } from "react-router-dom"
+import "./FavoriteCityCard.css"
 
-function FavoriteCityCard({city, time, description, temp}:FavoriteCityCardProps) {
+function FavoriteCityCard({
+  city,
+  time,
+  description,
+  temp
+}: FavoriteCityCardProps) {
   return (
-    <div className="favorite-city-card">
-      <div className="data-section">
-        <div className="city-name">{city}</div>
+    <NavLink to={'/'} className="favorite-city-card">
+        <div className="data-section">
+          <div className="city-name">{city}</div>
 
-        <div className="time">{time}</div>
+          <div className="time">{time}</div>
 
-        <div className="description">{description}</div>
-      </div>
+          <div className="description">{description}</div>
+        </div>
 
-      <div className="temp-section">{temp}&deg;C</div>
-    </div>
+        <div className="temp-section">{temp}&deg;C</div>
+    </NavLink>
   )
 }
 
