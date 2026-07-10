41cc0614e244c4d6b3ca65cbdd80c1f944c67566
Wesal Ismail <wsalasmayl363@gmail.com>
2026-07-10 03:57:21 +0300

    add: implement location fetching in WeatherSummary component

--- src/components/WeatherSummary.tsx
+++ src/components/WeatherSummary.tsx
@@ -1,5 +1,16 @@
+import axios from "axios"
 import { type WeatherSummaryProps } from "../types/weather"
+import { useEffect, useState } from "react"
 import "./WeatherSummary.css"
+
+const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY
+const currentWeatherBaseURL = import.meta.env.VITE_WEATHER_CURRENT_URL
+const geoURL = import.meta.env.VITE_WEATHER_GEO_URL
+
+type GeoData = {
+  lat: number
+  lon: number
+}
 
 function WeatherSummary({
   city,
@@ -8,7 +19,32 @@ function WeatherSummary({
   weatherDiscription,
   currentTime
 }: WeatherSummaryProps) {
+  const [currentWeatherData, setCurrentWeatherData] = useState<GeoData | null>(null)
+  const [location, setLocation] = useState<GeoData | null>(null)
   const isFavored = false
+
+ 
+  useEffect(() => {
+    const getLocationData = async () => {
+      const response = await axios(`${geoURL}?q=jeddah&appid=${weatherApiKey}`)
+      console.log(response.data[0].lat)
+      setLocation({
+        lat: response.data[0].lat,
+        lon: response.data[0].lon
+      })
+
+       console.log({
+        lat: response.data[0].lat,
+        lon: response.data[0].lon
+      })
+    } 
+
+  
+   
+
+  },[])
+
+  
 
   return (
     <div className="city-general-details">
