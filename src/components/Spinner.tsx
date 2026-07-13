5bedfd85e1505a725ed532e9ae4829d7adde2161
Wesal Ismail <wsalasmayl363@gmail.com>
2026-07-13 07:00:07 +0300

    add loading spinner to weatherdashboard

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import "./Spinner.css"
+
+function Spinner() {
+  return (
+    <div className="spinner-overlay">
+      <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Spinner;
