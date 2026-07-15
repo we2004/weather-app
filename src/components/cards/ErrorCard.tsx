a05f893de8687b0798db787505c8c89b9b326152
Wesal Ismail <wsalasmayl363@gmail.com>
2026-07-15 06:21:20 +0300

    feat: add ErrorCard component for displaying error message

---
+++ src/components/cards/ErrorCard.tsx
@@ -0,0 +1,14 @@
+import "./ErrorCard.css"
+
+function ErrorCard() {
+  return (
+    <div className="error-page">
+      <div className=" error-card">
+        <h2 className="error-title">Oops...</h2>
+        <p className="error-text">An error occurred. Please try again.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorCard
