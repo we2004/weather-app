e4697c30e6fae44fe1155df1b01d4fcecc08f296
Wesal Ismail <wsalasmayl363@gmail.com>
2026-07-15 05:43:10 +0300

    refactor: remove unused city management functions from Favorites.ts

--- src/utils/Favorites.ts
+++ src/utils/Favorites.ts
@@ -9,17 +9,3 @@ export const addCityToStorage = (citiesList: FavoriteCityData[]) => {
   localStorage.setItem("favoriteCities", JSON.stringify(citiesList))
 }
 
-//checks if a specific city exists or not
-export const checkCityExist = (cardCity: string): boolean => {
-  const favoriteCityList = getFavoriteCityList()
-  return favoriteCityList.some((cityInfo) => cityInfo.city === cardCity)
-}
-
-//remove city from favorites
-export const removeCityFromStorage = (
-  city: string,
-  citiesList: FavoriteCityData[]
-) => {
-  const newList = citiesList.filter((cityInfo) => cityInfo.city !== city)
-  addCityToStorage(newList)
-}
